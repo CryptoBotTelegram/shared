156661d7a214b46d8064ea3fc7637e47064f984e
karrad1201 <karad120109@gmail.com>
2025-08-22 14:47:51 +0500

    init shared lib

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from entity.promt import Prompt
-from entity.alert import Alert
-from entity.user import User
-
-
-from constants import ALERT_GENERAL_CONFIG_COINCIDENCE
-
-__all__ = ["Prompt", "Alert", "User", "ALERT_GENERAL_CONFIG_COINCIDENCE"]
---
+++ shared/__init__.py
@@ -0,0 +1,7 @@
+from shared.entity.promt import Prompt
+from shared.entity.alert import Alert
+from shared.entity.user import User
+
+from shared.constants import ALERT_GENERAL_CONFIG_COINCIDENCE
+
+__all__ = ["Prompt", "Alert", "User", "ALERT_GENERAL_CONFIG_COINCIDENCE"]
