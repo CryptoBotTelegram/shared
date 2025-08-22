7b8c2c4bd36e4bb2ea0daa41aaec49c2de941655
karrad1201 <karad120109@gmail.com>
2025-08-22 15:44:24 +0500

    init shared lib

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cryptobot-shared",
+    name="shared",
     version="0.1.1",
     packages=find_packages(include=['shared', 'shared.*']),
     include_package_data=True,
