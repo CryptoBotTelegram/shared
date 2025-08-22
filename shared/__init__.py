365e45962a81847de06793d2ec59edb8ae7e9af8
karrad1201 <karad120109@gmail.com>
2025-08-22 16:47:33 +0500

    init shared lib

--- shared/__init__.py
+++ shared/__init__.py
@@ -1,10 +1,10 @@
 from shared.shemas.promt import Prompt
 from shared.shemas.alert import Alert
-from shared.shemas.user import User, UserSettingsDTO
+from shared.shemas.user import User, UserSettingsDTO, UserFull
 
 from shared.constants import ALERT_GENERAL_CONFIG_COINCIDENCE
 
 from shared.shemas.telegram_bot import TelegramBotMode
 
-__all__ = ["Prompt", "Alert", "User", "ALERT_GENERAL_CONFIG_COINCIDENCE", "TelegramBotMode", "UserSettingsDTO"]
+__all__ = ["Prompt", "Alert", "User", "ALERT_GENERAL_CONFIG_COINCIDENCE", "TelegramBotMode", "UserSettingsDTO", "UserFull"]
 
